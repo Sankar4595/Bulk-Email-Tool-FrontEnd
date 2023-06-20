66706c38cb432d6cfc2ed25ec6a1a94c8f57bfb8
95sankar@gmail.com <95sankar@gmail.com>
2023-06-20 22:05:15 +0530

    url

--- src/url.js
+++ src/url.js
@@ -1,5 +1,5 @@
-// const API = "https://bulk-email-tool-backend-cu8v.onrender.com";
+const API = "https://bulk-email-tool-backend-cu8v.onrender.com";
 
-const API = "http://localhost:8600";
+// const API = "http://localhost:8600";
 
 export default API;
